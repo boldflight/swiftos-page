8ca10f7e7c8e7321e5abbfeadaa73c0b75233983
Douglas Hewitt <doughewitt4@gmail.com>
2025-09-29 15:52:03 +0900

    Update svelte.config.js
    add svelte fallback 404

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,13 @@ const config = {
     adapter: adapter({
       pages: "build",
       assets: "build",
-      fallback: undefined,
+      fallback: "404.html",
       precompress: false,
       strict: true,
     }),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/hummingbird.codes-svelte" : "",
+    },
     alias: {
       $lib: "src/lib",
     },
